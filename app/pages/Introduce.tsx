85ab864087868f812c0077a60f791172bd37ccdf
LEE2302 <dltpdudqkqh1@gmail.com>
2023-11-29 23:02:07 +0900

    프로필 삭제

--- app/pages/Introduce.tsx
+++ app/pages/Introduce.tsx
@@ -1,15 +1,15 @@
 import Content from "@/components/introduce/Content"
 import IntroTitle from "@/components/introduce/IntroTitle"
 import { BsMouse } from "react-icons/bs"
-// import Image from "next/image"
+import Image from "next/image"
 
 function Introduce() {
   return (
     <section id="intro" className="w-full h-[600px] text-center pt-[80px] bg-intro-image bg-cover bg-center relative">
       <div className="p-10 flex justify-center items-center relative text-white">
-        {/* <div className="mr-10 image-box">
+        <div className="mr-10 image-box">
           <Image src={"/images/profile/lee-profile.jpeg"} alt="profile" width={300} height={300} />
-        </div> */}
+        </div>
         <div>
           <IntroTitle />
           <Content />
