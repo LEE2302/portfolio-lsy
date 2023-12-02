0cf709dfbff23f8fab7780eda3ea586d2bc3e670
LEE2302 <dltpdudqkqh1@gmail.com>
2023-12-02 22:18:43 +0900

    intro 수정

--- components/introduce/Content.tsx
+++ components/introduce/Content.tsx
@@ -1,8 +1,8 @@
 function Content() {
   return (
     <div className="m-10 text-xs md:text-2xl break-keep">
-      <p className="text-xs md:text-2xl">무엇보다 개발에 흥미가 있으며 도전을 즐깁니다.</p>
-      <p className="text-xs md:text-2xl">뚜루루루뚜ㅜ뚜뚜뚠 개발하기가 좋아요</p>
+      <p className="text-xs md:text-2xl">구현해보고 싶은게 많습니다.</p>
+      <p className="text-xs md:text-2xl">도전을 피하지 않고 즐깁니다.</p>
       <p className="text-xs md:text-2xl">절망보단 성취감을 바라보며 나아갑니다.</p>
     </div>
   )
