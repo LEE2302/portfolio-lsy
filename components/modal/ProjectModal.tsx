7fcb53a23dc1600830f6188477d6a1be911ecce2
LEE2302 <dltpdudqkqh1@gmail.com>
2023-12-19 13:23:15 +0900

    프로젝트 모달 반응형 추가

--- components/modal/ProjectModal.tsx
+++ components/modal/ProjectModal.tsx
@@ -20,7 +20,7 @@ function ProjectModal({ open, onClose, modalRef, modalOutSideClick, children }: 
         ref={modalRef}
         onClick={e => modalOutSideClick(e)}
       />
-      <div className="scroll box1 prose max-w-[1032px] w-[1032px] h-[80%] overflow-y-scroll break-keep fixed top-[50%] left-[50%] p-[50px] bg-white z-50 translate-x-[-50%] translate-y-[-50%] rounded-md">
+      <div className=" scroll box1 prose max-w-[1032px] lg:w-[1032px] md:w-[800px] md:text-[100%] w-[350px] text-[60%]  h-[80%] overflow-y-scroll break-keep fixed top-[50%] left-[50%] p-[50px] bg-white z-50 translate-x-[-50%] translate-y-[-50%] rounded-md">
         <div>
           <button onClick={onClose}>모달 닫기</button>
         </div>
